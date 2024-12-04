dad7699e040a7e781e52bd036ef0074d0b87ad0d
Nay-Htet-Aung <blackbufflo17@gmail.com>
2024-12-04 18:32:57 +0630

    single-product page added and some minor changes in pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: [
+    "./*.html",
+    "./pages/*.html"
+  ],
   theme: {
     extend: {
       backgroundImage: {
